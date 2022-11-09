04daba0d475d7b37464ba42dd8bc3e1d9be3f060
ledatdz123 <lephuocdat0912a2@gmail.com>
2022-11-09 22:06:26 +0700

    updatemail

--- servercomputer/src/main/java/com/example/servercomputer/ServercomputerApplication.java
+++ servercomputer/src/main/java/com/example/servercomputer/ServercomputerApplication.java
@@ -1,6 +1,9 @@
 package com.example.servercomputer;
 
 import com.cloudinary.Cloudinary;
+import com.example.servercomputer.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,7 +15,9 @@ import java.util.Map;
 
 @SpringBootApplication
 @EnableSwagger2
-public class ServercomputerApplication {
+public class ServercomputerApplication /*implements CommandLineRunner*/{
+	@Autowired
+	private EmailService emailService;
 	@Value("${clouddinary.cloudName}")
 	public String cloudName;
 	@Value("${clouddinary.apiKey}")
@@ -25,6 +30,17 @@ public class ServercomputerApplication {
 //INSERT INTO `computer`.`role` (`id`, `role_name`) VALUES ('1', 'ROLE_USER');
 //INSERT INTO `computer`.`role` (`id`, `role_name`) VALUES ('2', 'ROLE_ADMIN');
 //INSERT INTO `computer`.`role` (`id`, `role_name`) VALUES ('3', 'ROLE_MANAGER');
+
+//@Override
+//public void run(String... args)
+//{
+//	//emailService.sendMail("lephuocdat0912a2@gmail.com", "Hi", "Ho ho ho");
+//	//emailService.sendPreConfiguredMail("Ho ho ho");
+//	emailService.sendMailWithAttachment("lephuocdat0912a2@gmail.com", "Hi",
+//			"<h1>Check attachment for image!</h1>",
+//			"<h1>Check attachment for image!</h1>");
+//}
+
 @Bean
 public Cloudinary cloudinary() {
 	Map<String, String> valueMap = new HashMap<>();
