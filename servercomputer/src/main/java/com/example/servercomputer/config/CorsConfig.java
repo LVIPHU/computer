d87c5adcf651645419ac412954a76a9f21b1e2c0
Lương Vĩ Phú <kenlock.lvp@gmail.com>
2022-12-01 02:32:09 +0700

    ax

--- servercomputer/src/main/java/com/example/servercomputer/config/CorsConfig.java
+++ servercomputer/src/main/java/com/example/servercomputer/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
 					.allowedOrigins(allowedOrigin)
-					.allowedMethods("GET")
+					.allowedMethods("*")
 					.allowedHeaders("*");
 			}
 		};
